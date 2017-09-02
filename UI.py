3864b11c30d82b80f237804148d1f0684bda8f9b
royabouhamad <roy.d.abouhamad@gmail.com>
2017-09-02 14:25:10 +0100

    check if image selected

--- UI.py
+++ UI.py
@@ -22,6 +22,7 @@ class Identifier(QMainWindow):
 
         self.label = QLabel(self)
         self.label.resize(500, 500)
+        self.label.setPixmap(QPixmap('images/NoImage.jpg').scaled(self.width(), self.height(), Qt.KeepAspectRatio))
         self.label.show()
 
         self.setGeometry(300, 300, 500, 600)
@@ -31,10 +32,14 @@ class Identifier(QMainWindow):
 
     def openImage(self):
         fName = QFileDialog.getOpenFileName(self, 'Open Image', '', 'Image Files (*.jpg *.jpeg)')
-        filePath = fName[0]
-        human_string, score = label(filePath)
-        self.line.setText(human_string + " " + str(round(score, 2)) + "%")
-        self.label.setPixmap(QPixmap(filePath).scaled(self.width(), self.height(), Qt.KeepAspectRatio))
+        try:
+            filePath = fName[0]
+            human_string, score = label(filePath)
+            self.line.setText(human_string + " " + str(round(score, 2)) + "%")
+            self.label.setPixmap(QPixmap(filePath).scaled(self.width(), self.height(), Qt.KeepAspectRatio))
+        except:
+            self.label.setPixmap(QPixmap('images/NoImage.jpg').scaled(self.width(), self.height(), Qt.KeepAspectRatio))
+            self.line.setText("No image selected!")
 
 def main():
     app = QApplication(sys.argv)
